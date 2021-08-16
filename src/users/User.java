f15d3772825bd0ce91a89b46fe34184ce7c23db7
Lucas Kaczynski Primon <lucaskaczyn@gmail.com>
2021-08-16 20:27:32 -0300

    removed constructor

--- src/users/User.java
+++ src/users/User.java
@@ -4,14 +4,8 @@ public class User {
    public String name;
    public String email;
    private String password;
-   
-   User(String name, String email, String password) {
-       this.name = name; 
-       this.email = email;
-       this.setPassword(password);
-   }
 
-   private String setPassword(String password) {
+   public String setPassword(String password) {
        return this.password = password;
    }
 }
