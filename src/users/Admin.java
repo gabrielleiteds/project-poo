fceb28a8d73ba5c9dda9d8b048b318acfa407936
Lucas Kaczynski Primon <lucaskaczyn@gmail.com>
2021-08-17 21:28:14 -0300

    Added method to list all users

--- src/users/Admin.java
+++ src/users/Admin.java
@@ -15,4 +15,27 @@ public class Admin extends User{
 
         return adm;
     }
+
+    public void getUsers(Admin a) {
+        ArrayList<User> db = new ArrayList<>();
+
+        ArrayList<Client> clients = Client.database;
+        for(Admin adm : database) {
+            User user = (User) adm;
+            db.add(user);
+        }
+        for(Client client : clients) {
+            User user = (User) client;
+            db.add(user);
+        }
+
+        for(User user : db) {
+            if(user != a) {
+                System.out.println();
+                System.out.println("------- " + user + " -------");
+                System.out.println("Nome: " + user.name);
+                System.out.println("Email: " + user.email);
+            }
+        }
+    }
 }
