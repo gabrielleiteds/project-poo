229297db4f7e25cdd12f5b0979d5e6f3536c2afd
Lucas Kaczynski Primon <lucaskaczyn@gmail.com>
2021-08-19 20:24:16 -0300

    Metodo para pegar estoques e ver perfis

--- src/users/Inspector.java
+++ src/users/Inspector.java
@@ -1,6 +1,9 @@
 package users;
 
 import java.util.ArrayList;
+import java.util.Scanner;
+
+import inventories.Stock;
 
 public class Inspector extends User {
     public static ArrayList<Inspector> database = new ArrayList<>();
@@ -17,5 +20,34 @@ public class Inspector extends User {
         database.add(this); 
 
         return this;
+    }
+
+    public void getClients() {
+        ArrayList<Client> clients = Client.database;
+
+        for(Client client : clients) {
+            System.out.println();
+            System.out.println("Nome: " + client.name);
+            System.out.println("Email: " + client.email);
+            System.out.println("Estoque: " + client.stock);
+            System.out.println("Quantidade de produtos: " + client.stock.food.size());
+        }
+    }
+
+    public void viewStock() {
+        Scanner scanner = new Scanner(System.in);
+        getClients();
+        String temail;
+
+        System.out.println("Digite o email do dono do estoque que deseja ver: ");
+        temail = scanner.nextLine();
+
+        ArrayList<Client> clients = Client.database;
+
+        for(Client client : clients) {
+            if(temail.equals(client.email)) {
+                Stock.getProducts(client);
+            }
+        }
     }
 }
